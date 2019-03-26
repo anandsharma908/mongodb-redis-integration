9146b8f722aef3c691e9dc48fa422a00d6cdf368
aks <anand0007sharma@gmail.com>
2019-03-26 15:37:27 +0530

    second commit

---
+++ src/main/java/com/aks/mongodbredisintegration/MongodbRedisIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.aks.mongodbredisintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbRedisIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbRedisIntegrationApplication.class, args);
+	}
+
+}
